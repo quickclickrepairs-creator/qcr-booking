735ab5aaaeef4985e5522fde7087659b17a89a6c
quickclickrepairs-creator <quickclickrepairs@gmail.com>
2026-01-03 18:12:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
@@ -9,26 +9,6 @@ templates = Jinja2Templates(directory="templates")
 async def home(request: Request):
     return templates.TemplateResponse("booking.html", {"request": request})
 
-@app.post("/book")
-async def book(
-    customer_name: str = Form(...),
-    customer_email: str = Form(...),
-    customer_phone: str = Form(...),
-    service_type: str = Form(...),
-    appointment_date: str = Form(...),
-    appointment_time: str = Form(...),
-    description: str = Form("")
-):
-    return HTMLResponse(f"""
-    <h1 style="color:green;text-align:center;margin-top:100px">Appointment Booked!</h1>
-    <h2 style="text-align:center">Thank you {customer_name}</h2>
-    <p style="text-align:center;font-size:24px">
-      Service: {service_type}<br>
-      Date: {appointment_date}<br>
-      Time: {appointment_time}<br>
-      We will contact you on {customer_phone}
-    </p>
-    <p style="text-align:center;margin-top:50px">
-      <a href="/">← Book Another</a>
-    </p>
-    """)
+@app.get("/admin")
+async def admin(request: Request):
+    return templates.TemplateResponse("admin_dashboard.html", {"request": request})
